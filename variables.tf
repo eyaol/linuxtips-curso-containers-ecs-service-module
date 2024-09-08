ad65c728d931646e54dfe022b753a251c573b803
Enio Lee <eniolee@MacBook-Air-de-Enio.local>
2024-09-08 14:35:30 -0300

    chore: criar task definition

--- variables.tf
+++ variables.tf
@@ -1,35 +1,48 @@
+variable "region" {
+
+}
+
 variable "service_name" {
-  type = string
+
 }
 
 variable "cluster_name" {
-  type = string
+
 }
 
 variable "vpc_id" {
-  type = string
+
 }
 
 variable "private_subnets" {
-  type = list(string)
+
 }
 
 variable "service_port" {
-  type = number
+
 }
 
 variable "service_cpu" {
-  type = number
+
 }
 
 variable "service_memory" {
-  type = number
+
 }
 
 variable "service_listener" {
-  type = string
+
 }
 
 variable "service_task_execution_role" {
-  type = string
+
 }
+
+variable "environment_variables" {
+
+}
+
+variable "capabilities" {
+
+}
+
